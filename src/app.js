8f431007afbf054703433b4893ce4514c447a82b
Gene Barra <Gebarra9@gmail.com>
2021-02-15 17:50:28 -0500

    Add: Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+// default fallback to 3000
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -110,6 +112,6 @@ app.get('*', (req, res) => {
 });
 
 //starting up a server is asynchronous
-app.listen(3000, () => {
-  console.log(`Server is up on port 3000.`);
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}.`);
 });
